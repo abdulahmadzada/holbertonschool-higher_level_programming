1be508b1f731b69a694c4ebd1d932bfedcfa9e46
Abdul <abdul.ahmedzade@gmail.com>
2025-06-18 19:47:09 +0000

    Initial commit

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,3 +5,13 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
     return new_dict
+
+def print_sorted_dictionary(a_dict):
+    for key in sorted(a_dict.keys()):
+        print(f"{key}: {a_dict[key]}")
+
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
