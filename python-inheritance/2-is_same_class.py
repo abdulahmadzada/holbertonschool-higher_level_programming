c587bf961b9660e8eb49ddc9813872526b3d389c
Abdul <abdul.ahmedzade@gmail.com>
2025-06-26 17:00:06 +0000

    Initial commit

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that checks exact class instance."""
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of the specified class.
+    
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+    
+    Returns:
+        bool: True if obj is exactly an instance of a_class, False otherwise
+    """
+    return type(obj) is a_class
