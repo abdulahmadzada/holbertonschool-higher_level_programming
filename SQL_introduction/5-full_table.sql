fb749dc4bb16fd46c92b416397227085450cc745
Abdul <abdul.ahmedzade@gmail.com>
2025-07-02 20:04:35 +0000

    Initial commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,6 @@
--- 4-show_table_description.sql
--- Script that prints the description of the table first_table from the current database
--- without using DESCRIBE or EXPLAIN statements
+-- 5-full_table.sql
+-- Script that prints the full CREATE TABLE statement for first_table in the current database
+-- without using DESCRIBE or EXPLAIN
 
--- GET table description from information_schema
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
+-- GET full CREATE TABLE statement
+SHOW CREATE TABLE first_table;
